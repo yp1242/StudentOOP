6befe6dd31a8ee2f864d74cbe956267607164229
yp1242 <yp1242@nyu.edu>
2019-06-05 12:13:14 -0400

    Update pointers.h

--- my_code/pointers.h
+++ my_code/pointers.h
@@ -1,18 +1,18 @@
+    
 #ifndef POINTERS_H
 #define POINTERS_H
 
 #include <string>
 #include <vector>
 
-//Hi...
 void inc_by_value(int n);
 void inc_with_pointer(int* n);
 void inc_with_reference(int& n);
 
 
 struct Thing {
-	int val;
-	Thing(int i) : val(i) {}
+    int val;
+    Thing(int i) : val(i) {}
 };
 
 Thing** create_array_of_things(int n);
@@ -21,12 +21,12 @@ void double_all_things(Thing** things, int n);
 void delete_all_things(Thing** things, int n);
 
 struct Student {
-	std::string name;
-	Student* ta;
-	Student(std::string nm) : name(nm), ta(nullptr) {}
+    std::string name;
+    Student* ta;
+    Student(std::string nm) : name(nm), ta(nullptr) {}
 };
 
 void assignTA(Student* s, Student* ta);
-void printTAs(std::vector<Student> students);
+void printTAs(std::vector<Student*>& students);
 
 #endif
