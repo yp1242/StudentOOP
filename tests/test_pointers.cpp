30507951f9675077a7ecda777a75d69da3137567
yp1242 <yp1242@nyu.edu>
2019-06-05 11:46:00 -0400

    Update test_pointers.cpp

--- tests/test_pointers.cpp
+++ tests/test_pointers.cpp
@@ -1,7 +1,8 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
 #include "../my_code/pointers.h"
-//Hi...
+
 using namespace std;
 
 const int NUM_THINGS = 40;
@@ -20,6 +21,17 @@ int main() {
 	cout << "After ref, n = " << n << endl;
 	assert(n == 2);
 
+	// now assign TAs to students
+	vector<Student*> students;
+	Student* mary = new Student("mary");
+	Student* gordon = new Student("gordon");
+	assignTA(mary, gordon);
+	students.push_back(mary);
+	students.push_back(gordon);
+	printTAs(students);
+	assert(mary->ta == gordon);
+	assert(gordon->ta == nullptr);
+
 	// now let's make, print, change, and delete some "things":
 	Thing** things = create_array_of_things(NUM_THINGS);
 	assert(things != nullptr);
@@ -33,8 +45,5 @@ int main() {
 	print_all_things(things, NUM_THINGS);
 
 	delete_all_things(things, NUM_THINGS);
-
-	// now assign TAs to students
-
 	return 0;
 }
