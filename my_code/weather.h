81650f67db7d743faf5c9c5147467984756ee2f5
yp1242 <yp1242@nyu.edu>
2019-06-06 12:05:37 -0400

    Create weather.h

---
+++ my_code/weather.h
@@ -0,0 +1,34 @@
+#ifndef WEATHER_H
+#define WEATHER_H
+
+#include <string>
+
+struct GPS {
+	double latitude;
+	double longitude;
+	GPS(double la, double lo) : latitude(la),
+		longitude(lo) {}
+};
+
+std::ostream& operator<<(std::ostream& os, const GPS& gps);
+
+const int UNRATED = -1;
+const int BAD = 0;
+const int OK = 1;
+const int GOOD = 2;
+
+class Weather {
+	friend std::ostream& operator<<(std::ostream& os, const Weather& w);
+public:
+	Weather(std::string nm, GPS loc);
+	std::string get_name() const;
+	int get_rating() const;
+	void set_rating(int new_rating);
+private:
+	std::string station_nm;
+	GPS my_loc;
+	int rating = UNRATED;
+};
+
+
+#endif
