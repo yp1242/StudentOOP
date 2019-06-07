3080254c3239300fe756ccb555ae58bde05282e0
yp1242 <yp1242@nyu.edu>
2019-06-07 12:21:38 -0400

    Update weather.cpp

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -7,6 +7,22 @@ using namespace std;
 /*
  * A constructor for weather class.
  * */
+
+Date::Date(int d, int m, int y) :day(d), month(m), year(y) {}
+
+ostream& operator<<(ostream& os, const Date& date){
+	os <<"Date: Day: "<<date.day << " Month: " << date.month << " Year: " << date.year << endl;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const WReading& wr) {
+	os << wr.date;
+	os << "Temperature: " << wr.temperature << " Humidity: " << wr.humidity << " Windspeed: " << wr.windspeed;
+	os << endl;
+	return os;
+
+}
+
 Weather::Weather(std::string nm, GPS loc) :
 	station_nm(nm), my_loc(loc) {
 }
@@ -24,13 +40,19 @@ void Weather::set_rating(int new_rating) {
 	rating = new_rating;
 }
 
+void Weather::add_reading(WReading wr) {
+	wreadings.push_back(wr);
+}
+
 ostream& operator<<(ostream& os, const GPS& gps) {
 	os << gps.latitude << gps.longitude;
 	return os;
 }
 ostream& operator<<(ostream& os, const Weather& w) {
-	int r = w.get_rating();
-	os << w.station_nm << " : rating" << r;
-	os << " My location is" << w.my_loc;
+	os <<"Name: "<< w.get_name() << "; rating: " << w.get_rating() << "; Location: " << w.my_loc;
+	os << endl;
+	for (WReading wr : w.wreadings) {
+		os << " " << wr << endl;
+	}
 	return os;
 }
