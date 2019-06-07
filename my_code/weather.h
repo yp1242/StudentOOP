edeaf09873e8b071efa382ba233688ffcb5cd398
yp1242 <yp1242@nyu.edu>
2019-06-07 12:21:41 -0400

    Update weather.h

--- my_code/weather.h
+++ my_code/weather.h
@@ -2,7 +2,8 @@
 #define WEATHER_H
 
 #include <string>
-//Hi...
+#include <vector>
+
 struct GPS {
 	double latitude;
 	double longitude;
@@ -12,19 +13,50 @@ struct GPS {
 
 std::ostream& operator<<(std::ostream& os, const GPS& gps);
 
+
+class Date {
+	friend std::ostream& operator<<(std::ostream& os, const Date& date);
+public:
+	Date(int d, int m, int y);
+private:
+	int day;
+	int month;
+	int year;
+};
+
+
+class WReading {
+	friend std::ostream& operator<<(std::ostream& os, const WReading& wr);
+public:
+	WReading(Date dt, double temp, double hum, double ws) :
+		date(dt), temperature(temp), humidity(hum), windspeed(ws)
+	{
+	}
+
+private:
+	Date date;
+	double temperature;  // stored temp in C
+	double humidity;
+	double windspeed;
+};
+
+
 const int UNRATED = -1;
 const int BAD = 0;
 const int OK = 1;
 const int GOOD = 2;
 
 class Weather {
+	// << should output name, rating, and use the GPS << to output my_loc
 	friend std::ostream& operator<<(std::ostream& os, const Weather& w);
 public:
 	Weather(std::string nm, GPS loc);
 	std::string get_name() const;
 	int get_rating() const;
 	void set_rating(int new_rating);
+	void add_reading(WReading wr);
 private:
+	std::vector<WReading> wreadings;
 	std::string station_nm;
 	GPS my_loc;
 	int rating = UNRATED;
