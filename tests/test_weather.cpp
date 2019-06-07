5db5de7ee2e00691b5c70c8854e9422eb36b559f
yp1242 <yp1242@nyu.edu>
2019-06-07 12:21:44 -0400

    Update test_weather.cpp

--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -1,10 +1,37 @@
 #include <cassert>
 #include <iostream>
+#include <fstream>
+#include "weather.h"
 
 using namespace std;
 
+string get_input_file() {
+	string filenm;
+	cout << "Input the name of the new readings file:\n";
+	cin >> filenm;
+	return filenm;
+}
+
+void get_wreadings(Weather& w) {
+	ifstream rfile(get_input_file());
+	while (!rfile) {
+		cout << "Could not read input file." << endl;
+		rfile.open(get_input_file());
+	}
+	int m, d, y;
+	double temp, hum, ws;
+	while (rfile >> m >> d >> y >> temp >> hum >> ws) {
+		WReading wr = WReading(Date(d, m, y), temp, hum, ws);
+		w.add_reading(wr);
+	}
+	rfile.close();
+}
+
+
 int main() {
-	GPS loc = GPS(46.3, 67.2);
-	Weather irkutsk = Weather("Irkutsk", loc);
+	Weather irkutsk = Weather("Irkutsk", GPS(46.3, 67.2));
+
+	get_wreadings(irkutsk);
+
 	cout << irkutsk << endl;
 }
